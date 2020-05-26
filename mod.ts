90c76a4af6bf07d7a4c8fa3b65b3dfd3a15234a0
Maximous Black <maximousblk@gmail.com>
2020-05-26 22:44:53 +0530

    added a check for module name

--- mod.ts
+++ mod.ts
@@ -3,35 +3,37 @@ import { parse } from "https://deno.land/std/flags/mod.ts";
 const args = parse(Deno.args);
 
 export default async function audit(
-  module: string,
-  database: string,
-  currentVersion: string,
+    moduleName: string,
+    database: string,
+    currentVersion: string
 ) {
-  if (args.audit === true) {
-    if (!database) {
-      console.log("Audit database not provided");
-    } else if (!currentVersion) {
-      console.log("Current Version not provided");
-    } else {
-      var versiondb = await fetch(database).then((data) => {
-        return data.json();
-      });
-      versiondb.versions.forEach((version: any) => {
-        if (currentVersion == version.tag) {
-          let patch = "",
-            note = "";
+    if (args.audit === true) {
+        if (!moduleName) {
+            console.log("Module name not provided");
+        } else if (!database) {
+            console.log("Audit database URL not provided");
+        } else if (!currentVersion) {
+            console.log("Current Version of the module not provided");
+        } else {
+            var versiondb = await fetch(database).then((data) => {
+                return data.json();
+            });
+            versiondb.versions.forEach((version: any) => {
+                if (currentVersion == version.tag) {
+                    let patch = "",
+                        note = "";
 
-          if (version.patch) {
-            patch = ` Patch: ${version.patch} |`;
-          }
-          if (version.note) {
-            note = ` Note: ${version.note} |`;
-          }
-          console.log(
-            `| Insecure: ${module} | version: ${version.tag} |${patch}${note}`,
-          );
+                    if (version.patch) {
+                        patch = ` Patch: ${version.patch} |`;
+                    }
+                    if (version.note) {
+                        note = ` Note: ${version.note} |`;
+                    }
+                    console.log(
+                        `| Insecure: ${moduleName} | version: ${version.tag} |${patch}${note}`
+                    );
+                }
+            });
         }
-      });
     }
-  }
 }
